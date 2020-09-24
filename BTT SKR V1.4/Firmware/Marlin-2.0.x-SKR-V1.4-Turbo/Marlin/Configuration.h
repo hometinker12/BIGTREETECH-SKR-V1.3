b0176b10426db426efee159abdf12ef340e8d44f
hometinker12 <48232506+hometinker12@users.noreply.github.com>
2020-09-24 09:39:48 -0400

    Before New Version

--- BTT SKR V1.4/Firmware/Marlin-2.0.x-SKR-V1.4-Turbo/Marlin/Configuration.h
+++ BTT SKR V1.4/Firmware/Marlin-2.0.x-SKR-V1.4-Turbo/Marlin/Configuration.h
@@ -1092,7 +1092,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 380  
+#define X_BED_SIZE 375  
 #define Y_BED_SIZE 345
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
